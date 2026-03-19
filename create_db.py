de97eeadbe024d77147d198aa8d481d3f63bc69a
florencygajera <florencygajera003@gmail.com>
2026-03-19 10:29:46 +0530

    feat: introduce script for database initialization and table creation for SQLite.

---
+++ create_db.py
@@ -0,0 +1,16 @@
+"""Script to create SQLite database tables"""
+import os
+os.environ['USE_SQLITE'] = 'true'
+
+from common.core.database import import_models, Base, get_db_engine
+import asyncio
+
+async def create_db():
+    import_models()
+    engine = get_db_engine()
+    print(f'Using database: {engine.url}')
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print('Database tables created successfully!')
+
+asyncio.run(create_db())
